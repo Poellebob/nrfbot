b1d8b2aae33e6ec1aa5d9a53d97a3c07c90798e3
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2026-01-15 16:54:29 +0100

    hehe

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,16 @@
 #define MTR1b 6
 #define MTR1e 3
 
-#define SL 13
-#define SR 12
-#define SML 11
-#define SM 10
-#define SMR 9
+#define SL 9
+#define SR 13
+#define SML 10
+#define SM 11
+#define SMR 12
 
 Car self = Car(MTR0f, MTR1f, MTR0b, MTR1b, MTR0e, MTR1e);
+LineFollower lineFollower = LineFollower(self, SL, SML, SM, SMR, SR);
+
+bool isLineFollower = false;
 
 // NRF24 pins
 #define CE_PIN 48
@@ -34,6 +37,8 @@ int16_t payload[9];
 void setup() {
   Serial.begin(9600);
   delay(100);
+
+  lineFollower.init();
 
   if (!radio.begin()) {
     Serial.println(F("nRF24 hardware not responding!"));
@@ -60,11 +65,36 @@ void setup() {
 
 void loop() {
   if (radio.available()) {
-    radio.read(&payload, sizeof(payload));
-    Serial.print(payload[0]);
-    Serial.println(payload[1]);
+    int last6 = payload[6];
 
-    self.drive(payload[0], payload[1]);
+    if (!isLineFollower) {
+      radio.read(&payload, sizeof(payload));
+      Serial.print(payload[0]);
+      Serial.print(", ");
+      Serial.print(payload[1]);
+      Serial.print(", ");
+      Serial.print(payload[2]);
+      Serial.print(", ");
+      Serial.print(payload[3]);
+      Serial.print(", ");
+      Serial.print(payload[4]);
+      Serial.print(", ");
+      Serial.print(payload[5]);
+      Serial.print(", ");
+      Serial.print(payload[6]);
+      Serial.print(", ");
+      Serial.print(payload[7]);
+      Serial.print(", ");
+      Serial.println(payload[8]);
+    }
+
+    if (last6 != payload[6] && payload[6] == 0)
+      isLineFollower = !isLineFollower;
+
+    if (isLineFollower)
+      lineFollower.run();
+    else
+      self.drive(payload[0], payload[1]);
   }
   delay(10);
 }
--- src/motor.h
+++ src/motor.h
@@ -1,21 +1,19 @@
 #include <Arduino.h>
+#include <Wire.h>
 
 class Motor {
 public:
   uint8_t forward;
   uint8_t backward;
   uint8_t enable;
-
   Motor(uint8_t pin_forward, uint8_t pin_backward, uint8_t pin_enable) {
     forward = pin_forward;
     backward = pin_backward;
     enable = pin_enable;
-
     pinMode(forward, OUTPUT);
     pinMode(backward, OUTPUT);
     pinMode(enable, OUTPUT);
   }
-
   void setSpeed(int speed) {
     speed = constrain(speed, -255, 255);
     digitalWrite(enable, true);
@@ -30,48 +28,168 @@ public:
       analogWrite(forward, speed);
     }
   }
-
   void setEnable(bool _enable) { digitalWrite(enable, _enable); }
 };
 
 class Car {
 public:
-  Motor motorL;
-  Motor motorR;
-
   int centerX = 512;
   int centerY = 512;
   int deadzone = 40;
+  Motor motorL;
+  Motor motorR;
 
   Car(uint8_t r_pin_forward, uint8_t l_pin_forward, uint8_t r_pin_backward,
       uint8_t l_pin_backward, uint8_t r_pin_enable, uint8_t l_pin_enable)
       : motorL(l_pin_forward, l_pin_backward, l_pin_enable),
         motorR(r_pin_forward, r_pin_backward, r_pin_enable) {}
 
-  void drive(int joystick_x, int joystick_y) {
-    int x = joystick_x - centerX; // steering
-    int y = joystick_y - centerY; // forward/back
-
+  void drive(int steer = 0, int speed = 0) {
+    int x = steer - centerX; // steering
+    int y = speed - centerY; // forward/back
     if (abs(x) < deadzone)
       x = 0;
     if (abs(y) < deadzone)
       y = 0;
-
     int forward = map(y, -512, 512, -255, 255);
     int turn = map(x, -512, 512, -255, 255);
-
     int leftSpeed = forward - turn;
     int rightSpeed = forward + turn;
-
     leftSpeed = constrain(leftSpeed, -255, 255);
     rightSpeed = constrain(rightSpeed, -255, 255);
-
     motorL.setSpeed(leftSpeed);
     motorR.setSpeed(rightSpeed);
   }
-
   void stop() {
     motorL.setEnable(false);
     motorR.setEnable(false);
   }
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    motorL.setSpeed(leftSpeed);
+    motorR.setSpeed(rightSpeed);
+  }
+};
+
+class LineFollower {
+private:
+  Car &car;
+  uint8_t sensorLeftOuter;
+  uint8_t sensorLeftInner;
+  uint8_t sensorCenter;
+  uint8_t sensorRightInner;
+  uint8_t sensorRightOuter;
+  bool leftOuter;
+  bool leftInner;
+  bool center;
+  bool rightInner;
+  bool rightOuter;
+  int baseSpeed = 0;
+  char path[100];
+  int pathLength = 0;
+  int rot = 0;
+
+#define L -1
+#define R 1
+#define S 0
+#define ERRC 70
+
+  int dir = S;
+  int errc = 0;
+
+  int16_t gyro_x, gyro_y, gyro_z;
+#define MPU_ADDR 0x68
+
+  // Intersection detection
+  unsigned long lastIntersectionTime = 0;
+  const unsigned long intersectionDelay = 300; // ms between intersections
+
+public:
+  LineFollower(Car &_car, uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3,
+               uint8_t s4, int speed = 150)
+      : car(_car), sensorLeftOuter(s0), sensorLeftInner(s1), sensorCenter(s2),
+        sensorRightInner(s3), sensorRightOuter(s4) {
+    pinMode(sensorLeftOuter, INPUT);
+    pinMode(sensorLeftInner, INPUT);
+    pinMode(sensorCenter, INPUT);
+    pinMode(sensorRightInner, INPUT);
+    pinMode(sensorRightOuter, INPUT);
+
+    setBaseSpeed(speed);
+  }
+
+  void init() {
+    Wire.begin();
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x6B);
+    Wire.write(0);
+    Wire.endTransmission();
+  }
+
+  // Read all sensors (false = black/line, true = white)
+  void readSensors() {
+    leftOuter = digitalRead(sensorLeftOuter);
+    leftInner = digitalRead(sensorLeftInner);
+    center = digitalRead(sensorCenter);
+    rightInner = digitalRead(sensorRightInner);
+    rightOuter = digitalRead(sensorRightOuter);
+  }
+
+  void getDir() {
+    if (leftOuter && rightOuter) {
+      path[pathLength] = S;
+      pathLength++;
+      dir = S;
+      return;
+    } else if (!leftOuter && rightOuter) {
+      path[pathLength] = S;
+      pathLength++;
+      dir = S;
+    } else if (leftOuter && !rightOuter) {
+      path[pathLength] = R;
+      pathLength++;
+      dir = R;
+    } else if (!leftOuter && !rightOuter) {
+      path[pathLength] = R;
+      pathLength++;
+      dir = R;
+    }
+
+    if (dir != S)
+      return;
+
+    if (leftInner && rightInner) {
+      errc = 0;
+    } else if (leftInner && !rightInner) {
+      errc = ERRC;
+    } else if (!leftInner && rightInner) {
+      errc = -ERRC;
+    }
+  }
+
+  // Main run loop
+  void run() {
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, 3 * 2, true);
+
+    gyro_x = Wire.read() << 8 | Wire.read();
+    gyro_y = Wire.read() << 8 | Wire.read();
+    gyro_z = Wire.read() << 8 | Wire.read();
+
+    char tmp_str[7];
+    sprintf(tmp_str, "%6d", gyro_y);
+    Serial.println(tmp_str);
+
+    readSensors();
+    getDir();
+
+    int turn_rot = 0;
+    if (turn_rot >= 90)
+      dir = S;
+
+    car.drive(baseSpeed * dir, baseSpeed);
+  }
+
+  void setBaseSpeed(int speed) { baseSpeed = constrain(speed, 0, 255); }
 };
